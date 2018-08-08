81cfc682c2dbb61cc537f461ee87693c7c8f58b3
Tim Musharapov <tmusharapov@bk.ru>
2018-08-08 18:55:50 +0300

    Init commit

---
+++ Assets/GetMonye.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMonye : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/ImageTargetPlayAudio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAudio : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            audioSource.Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            if (audioSource != null)
+                audioSource.Stop();
+        }
+    }
+}
